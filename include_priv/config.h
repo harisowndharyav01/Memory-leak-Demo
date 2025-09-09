24fc60f3bd4f825fd7dd5f170a4bec7751d2f202
Harisowndharya V <147607204+harisowndharyav01@users.noreply.github.com>
2025-09-09 16:06:14 +0530

    Create config.h

---
+++ include_priv/config.h
@@ -0,0 +1,26 @@
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `c' library (-lc). */
+#define HAVE_LIBC 1
+
+/* Name of package */
+#define PACKAGE "memoryleak"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "memoryleak"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "memoryleak 1.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "memoryleak"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.2"
+
+/* Version number of package */
+#define VERSION "0.1"
